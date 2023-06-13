74d1459109e2b7afa9961883e61c6940cfd62472
adayaTovim <adaya012@gamail.com>
2023-06-13 15:27:19 +0300

    remove print from the code

--- interview_questions/nums_to_words.cpp
+++ interview_questions/nums_to_words.cpp
@@ -21,25 +21,21 @@ std::string numsToString(size_t a_num)
 
     if (a_num >= 1000000) {
         result += belowTwenty[a_num / 1000000] + " Million ";
-		std::cout<<result<<std::endl;
         a_num %= 1000000;
     }
 
     if (a_num >= 1000) {
         result += numsToString(a_num / 1000) + " Thousand ";
-		std::cout<<result<<std::endl;
         a_num %= 1000;
     }
 
     if (a_num >= 100) {
         result += belowTwenty[a_num / 100] + " Hundred ";
-		std::cout<<result<<std::endl;
         a_num %= 100;
     }
 
     if (a_num >= 20) {
         result += tens[a_num / 10] + " ";
-		std::cout<<result<<std::endl;
         a_num %= 10;
     }
 
@@ -53,6 +49,7 @@ std::string numsToString(size_t a_num)
 int main()
 {
 	std::cout << "103: " << numsToString(103) << std::endl;
+	std::cout << "12000000: " << numsToString(12000000) << std::endl;
 	std::cout << "1234: " << numsToString(1234) << std::endl;
     //std::cout << "12345: " << numsToString(12345) << std::endl;
     //std::cout << "1234567: " << numsToString(1234567) << std::endl;
