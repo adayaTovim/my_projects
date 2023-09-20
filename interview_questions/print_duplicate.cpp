ac68c969289eb77bb25da696dcdbdee79ae3fcfe
adayaTovim <adaya012@gamail.com>
2023-09-20 12:02:59 +0300

    add function that print unique num

--- interview_questions/print_duplicate.cpp
+++ interview_questions/print_duplicate.cpp
@@ -7,7 +7,11 @@
 
 void printRepeatingNums(const std::vector<int>& a_vec)
 {
-    std::unordered_map<int,int> map;
+    if(a_vec.size() < 2) {
+        return;
+    }
+
+	std::unordered_map<int,int> map;
     for (auto num : a_vec) {
         map[num]++;
     }
@@ -19,10 +23,40 @@ void printRepeatingNums(const std::vector<int>& a_vec)
     }
 }
 
+//naive way
+// void printRepeatingNums(const std::vector<int>& a_vec)
+// {
+//     int size = a_vec.size();
+//     for(int i = 0; i < size; ++i) {
+//         bool isRepeated = false;
+//         for (int j = i + 1; j < size; ++j) {
+//             if(a_vec[i] == a_vec[j]) {
+//                 isRepeated = true;
+//             }
+//         }
+//         if(isRepeated) {
+//             std::cout<<a_vec[i]<<" ";
+//         }
+//     }
+// }
+
+//function that print unique num , itis work only if there is only one unique
+void findUniqueNum(const std::vector<int>& a_vec)
+{
+    int result = 0;
+    for (auto num : a_vec) {
+        result ^= num;
+    }
+    std::cout<<result<<std::endl;
+}
+
 int main()
 {
     std::vector<int> v = {1,2,3,1,1,4,5,6,7,3,8,10};
     printRepeatingNums(v);
+
+	std::vector<int> v2 = {1,2,3,1,2};
+    //findUniqueNum(v2);
     return 0;
 }
 
